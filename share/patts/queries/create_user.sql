4b084d098300e573ae81e9fc17caafdd7a1d718a
Gamah <deadon22087@gmail.com>
2014-09-29 20:00:11 -0500

    Update create_user.sql

--- share/patts/queries/create_user.sql
+++ share/patts/queries/create_user.sql
@@ -1,8 +1,4 @@
-
-function create_user($username, $password, $firstname, $lastname)
-	{
 	CREATE USER '$username'@'%' identified by '$password';
 	GRANT INSERT, SELECT, UPDATE ON pattsdb.* TO '$username'@'%';
 	INSERT INTO pattsdb.User(State,IsAdmin,MySQLUser,FirstName,LastName) VALUES(1,0,'$username','$firstname','$lastname');
 	FLUSH PRIVILEGES;
-	}
