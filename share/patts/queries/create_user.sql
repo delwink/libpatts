529c3965fd9887050c9c84c84fab932e17eed180
Gamah <deadon22087@gmail.com>
2014-10-08 18:54:24 -0500

    scripts directory

--- share/patts/queries/create_user.sql
+++
@@ -1,4 +0,0 @@
-	CREATE USER '$username'@'%' identified by '$password';
-	GRANT INSERT, SELECT, UPDATE ON pattsdb.* TO '$username'@'%';
-	INSERT INTO pattsdb.User(State,IsAdmin,MySQLUser,FirstName,LastName) VALUES(1,0,'$username','$firstname','$lastname');
-	FLUSH PRIVILEGES;
