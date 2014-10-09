7f8fbd9d3873e94ac1165024d11b33523351ed80
Gamah <deadon22087@gmail.com>
2014-10-08 19:37:00 -0500

    added the query back

--- scripts/client/create_user.sql
+++ scripts/client/create_user.sql
@@ -1,1 +1,4 @@
-selecT * from TaskItem
+CREATE USER '$username'@'%' identified by '$password';
+GRANT INSERT, SELECT, UPDATE ON pattsdb.* TO '$username'@'%';
+INSERT INTO pattsdb.User(state,isAdmin,MysqlUser,firstName,lastName) VALUES(1,0,'$username','$firstname','$lastname');
+FLUSH PRIVILEGES;
