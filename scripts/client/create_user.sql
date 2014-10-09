8e9a338172f76d3d8e31b747e7b8480ceb50ac29
Gamah <deadon22087@gmail.com>
2014-10-08 19:37:45 -0500

    Update create_user.sql

--- scripts/client/create_user.sql
+++ scripts/client/create_user.sql
@@ -1,4 +1,4 @@
 CREATE USER '$username'@'%' identified by '$password';
 GRANT INSERT, SELECT, UPDATE ON pattsdb.* TO '$username'@'%';
-INSERT INTO pattsdb.User(state,isAdmin,MysqlUser,firstName,lastName) VALUES(1,0,'$username','$firstname','$lastname');
+INSERT INTO pattsdb.User(state,isAdmin,MysqlUser,firstName,lastName) VALUES(1,$adminbit,'$username','$firstname','$lastname');
 FLUSH PRIVILEGES;
