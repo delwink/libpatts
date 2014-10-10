ba5dce550ad12586cc3d8a7344a089ababf25eca
Gamah <deadon22087@gmail.com>
2014-10-09 21:00:49 -0500

    new init script

--- scripts/server/init.sql
+++ scripts/server/init.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE IF NOT EXISTS pattsdb;
+
 CREATE TABLE `pattsdb`.`User`
 	(
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -9,14 +10,7 @@ CREATE TABLE `pattsdb`.`User`
 	`mysqlUser` VARCHAR(45) NOT NULL,
 	PRIMARY KEY (`id`)
 	);
-CREATE TABLE `pattsdb`.`Client` 
-	(
-	`id` INT SIGNED NOT NULL AUTO_INCREMENT,
-	`state` BIT NOT NULL,
-	`firstName` VARCHAR(45) NOT NULL,
-	`lastName` VARCHAR(45) NOT NULL,
-	PRIMARY KEY (`id`)
-	);
+
 CREATE TABLE `pattsdb`.`TaskType` 
 	(
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -25,6 +19,7 @@ CREATE TABLE `pattsdb`.`TaskType`
 	`displayName` VARCHAR(45),
 	PRIMARY KEY (`id`)
 	);
+
 CREATE TABLE `pattsdb`.`TaskItem` 
 	(
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -37,3 +32,8 @@ CREATE TABLE `pattsdb`.`TaskItem`
 	`endTime` DATETIME NULL,
 	PRIMARY KEY(`id`)
 	);
+
+CREATE USER 'patts'@'%' IDENTIFIED BY 'patts';
+GRANT ALL PRIVILEGES ON *.* TO 'patts'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+INSERT INTO pattsdb.User VALUES(1,1,'Patts','Admin','patts');
