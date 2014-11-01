fa71a4d07c9997ce3083a87b2b03c9567b31f75e
David McMackins II <contact@mcmackins.org>
2014-11-01 10:27:18 -0500

    Integrate with cquel

--- include/patts/get.h
+++ include/patts/get.h
@@ -1,8 +1,10 @@
 /*
+ *  libpatts - Backend library for PATTS Ain't Time Tracking Software
+ *  Copyright (C) 2014 Delwink, LLC
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ *  the Free Software Foundation.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,24 +18,24 @@
 #ifndef DELWINK_PATTS_GET_H
 #define DELWINK_PATTS_GET_H
 
+#include <cquel.h>
+
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-#include "query.h"
+int patts_get_users(struct dbconn con, struct dlist **out);
+int patts_get_user_byid(struct dbconn con, struct dlist **out, const char *id);
 
-int patts_get_users(struct dbconn con, struct dlist *out);
-int patts_get_user_byid(struct dbconn con, struct dlist *out, const char *id);
+int patts_get_types(struct dbconn con, struct dlist **out);
+int patts_get_type_byid(struct dbconn con, struct dlist **out, const char *id);
 
-int patts_get_types(struct dbconn con, struct dlist *out);
-int patts_get_type_byid(struct dbconn con, struct dlist *out, const char *id);
-
-int patts_get_items(struct dbconn con, struct dlist *out);
-int patts_get_item_byid(struct dbconn con, struct dlist *out, const char *id);
-int patts_get_items_byuser(struct dbconn con, struct dlist *out,
+int patts_get_items(struct dbconn con, struct dlist **out);
+int patts_get_item_byid(struct dbconn con, struct dlist **out, const char *id);
+int patts_get_items_byuser(struct dbconn con, struct dlist **out,
         const char *userID);
-int patts_get_items_byuser_onclock(struct dbconn con, struct dlist *out,
+int patts_get_items_byuser_onclock(struct dbconn con, struct dlist **out,
         const char *userID);
 
 #ifdef __cplusplus
--- src/admin.c
+++ src/admin.c
@@ -1,8 +1,10 @@
 /*
+ *  libpatts - Backend library for PATTS Ain't Time Tracking Software
+ *  Copyright (C) 2014 Delwink, LLC
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ *  the Free Software Foundation.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
