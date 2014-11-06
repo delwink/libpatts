1e9e89354e1ac01d6e718fcfbdc1f8695973b140
David McMackins II <contact@mcmackins.org>
2014-11-06 07:05:02 -0600

    Implemented user fetchers

--- src/get.c
+++ src/get.c
@@ -14,3 +14,31 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "patts.h"
+#include "get.h"
+
+int patts_get_users(struct dlist **out)
+{
+    return cq_select_all(patts_get_db(), u8"User", out, u8"");
+}
+
+int patts_get_user_byid(struct dlist **out, const char *id)
+{
+    if (strlen(id) >= patts_fmaxlen() - strlen(u8"id="))
+        return 1;
+
+    char *s = calloc(patts_fmaxlen(), sizeof(char));
+    if (s == NULL)
+        return -1;
+
+    strcat(s, id);
+
+    int rc = cq_select_all(patts_get_db(), u8"User", out, s);
+    free(s);
+
+    return rc;
+}
