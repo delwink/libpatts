0d351af7918640cba5f1ab94995aa6da5605558d
David McMackins II <contact@mcmackins.org>
2014-11-24 20:09:15 -0600

    Add internal function for setting state of a table transaction

--- src/admin.c
+++ src/admin.c
@@ -14,3 +14,39 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+#include <string.h>
+
+#include "admin.h"
+#include "get.h"
+#include "patts.h"
+
+static int set_state(const char *table, const char *id, const char *state)
+{
+    if (id == NULL)
+        return 1;
+    if (patts_fmaxlen() < 2) /* impending buffer overflow */
+        return -1;
+
+    struct dlist *list;
+    int rc = patts_get_user_byid(&list, id);
+    if (rc)
+        return rc;
+    if (cq_dlist_size(list) != 1) {
+        cq_free_dlist(list);
+    }
+
+    size_t index;
+    rc = cq_field_to_index(list, u8"state", &index);
+    if (rc) {
+        cq_free_dlist(list);
+        return rc;
+    }
+
+    strcpy(list->first->values[index], state);
+
+    rc = cq_update(patts_get_db(), table, list);
+    cq_free_dlist(list);
+
+    return rc;
+}
