4ea393219f979c3ef500a673438f1159968ee305
David McMackins II <contact@mcmackins.org>
2014-12-06 11:16:01 -0600

    Implemented child items getter

--- src/get.c
+++ src/get.c
@@ -184,3 +184,83 @@ int patts_get_items_byuser_onclock(struct dlist **out, const char *userID)
 
     return rc;
 }
+
+int patts_get_child_items(struct dlist **out, const char *id)
+{
+    if (id == NULL)
+        return 1;
+
+    char *s = calloc(patts_qlen(), sizeof(char));
+    if (s == NULL)
+        return -1;
+
+    int rc = sprintf(s, "%s%s%s", 
+            u8"typeID,userID,startTime,stopTime FROM TaskItem WHERE ", 
+            u8"id=", id);
+    if ((size_t)rc >= patts_qlen()) {
+        free(s);
+        return 2;
+    }
+
+    struct dlist *parent;
+    rc = cq_select_query(patts_get_db(), &parent, s);
+    if (rc) {
+        free(s);
+        return rc;
+    }
+
+    const char *typeID = parent->first->values[0],
+            *userID = parent->first->values[1],
+            *startTime = parent->first->values[2],
+            *stopTime = parent->first->values[3];
+
+    struct dlist *child_types;
+    rc = patts_get_child_types(&child_types, typeID);
+    if (rc) {
+        cq_free_dlist(parent);
+        free(s);
+        return rc;
+    }
+
+    size_t tid_index;
+    rc = cq_field_to_index(child_types, u8"id", &tid_index);
+    if (rc) {
+        cq_free_dlist(parent);
+        cq_free_dlist(child_types);
+        free(s);
+        return 100;
+    }
+
+    char *tids = calloc((patts_fmaxlen() + strlen(u8",")) 
+            * cq_dlist_size(child_types), sizeof(char));
+    if (tids == NULL) {
+        cq_free_dlist(parent);
+        cq_free_dlist(child_types);
+        free(s);
+        return -3;
+    }
+
+    for (struct drow *row = child_types->first; row != NULL; row = row->next) {
+        strcat(tids, row->values[tid_index]);
+        if (row->next != NULL)
+            strcat(tids, u8",");
+    }
+
+    rc = sprintf(s, "%s%s%s%s%s%s%s%s%s%s%s", 
+            u8"startTime>=", startTime, u8",",
+            u8"stopTime<=", stopTime, u8",",
+            u8"typeID IN (", tids, u8"),",
+            u8"userID=", userID);
+    cq_free_dlist(parent);
+    cq_free_dlist(child_types);
+    free(tids);
+    if ((size_t)rc >= patts_qlen()) {
+        free(s);
+        return 101;
+    }
+
+    rc = cq_select_all(patts_get_db(), u8"TaskItem", out, s);
+    free(s);
+
+    return rc;
+}
