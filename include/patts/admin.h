e4a58127ae7cc71f0777d292f021cf99d34f225a
David McMackins II <contact@mcmackins.org>
2015-02-15 18:02:55 -0600

    Rewrite admin functions for SQON

--- include/patts/admin.h
+++ include/patts/admin.h
@@ -1,6 +1,6 @@
 /*
  *  libpatts - Backend library for PATTS Ain't Time Tracking Software
- *  Copyright (C) 2014 Delwink, LLC
+ *  Copyright (C) 2014-2015 Delwink, LLC
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published by
@@ -18,22 +18,28 @@
 #ifndef DELWINK_PATTS_ADMIN_H
 #define DELWINK_PATTS_ADMIN_H
 
-#include <cquel.h>
-
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-int patts_create_user(struct dlist *info, const char *host,
-        const char *passwd);
-int patts_create_task(struct dlist *info);
+int
+patts_create_user (const char *id, const char *host, const char *passwd);
 
-int patts_delete_user(const char *id);
-int patts_delete_task(const char *id);
+int
+patts_create_task (const char *parentID, const char *displayName);
 
-int patts_grant_admin(const char *id);
-int patts_revoke_admin(const char *id);
+int
+patts_delete_user (const char *id);
+
+int
+patts_delete_task (const char *id);
+
+int
+patts_grant_admin (const char *id);
+
+int
+patts_revoke_admin (const char *id);
 
 #ifdef __cplusplus
 }
--- src/admin.c
+++ src/admin.c
@@ -1,6 +1,6 @@
 /*
  *  libpatts - Backend library for PATTS Ain't Time Tracking Software
- *  Copyright (C) 2014 Delwink, LLC
+ *  Copyright (C) 2014-2015 Delwink, LLC
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published by
@@ -18,138 +18,130 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sqon.h>
 
 #include "admin.h"
-#include "get.h"
 #include "patts.h"
 
-int patts_create_user(struct dlist *info, const char *host,
-        const char *passwd)
+int
+patts_create_user (const char *id, const char *host, const char *passwd)
 {
-    int rc;
-    struct dlist *newid = NULL;
+  int rc;
+  const char *fmt = "SELECT createUser('%s','%s','%s')";
+  char *query;
+  size_t qlen = 1;
 
-    size_t index;
-    rc = cq_field_to_index(info, u8"mysqlUser", &index);
-    if (rc)
-        return 1;
+  qlen += strlen (fmt) - 6;
+  qlen += strlen (id);
+  qlen += strlen (host);
+  qlen += strlen (passwd);
 
-    const char *args[] = {
-        info->first->values[index],
-        host,
-        passwd
-    };
+  query = sqon_malloc (qlen * sizeof (char));
+  if (NULL == query)
+    return PATTS_MEMORYERROR;
 
-    rc = cq_select_func_arr(patts_get_db(), u8"createUser", args, 1, &newid);
-    if (rc)
-        return 2;
+  snprintf (query, qlen, fmt, id, host, passwd);
 
-    rc = cq_field_to_index(info, u8"id", &index);
-    if (rc) {
-        cq_free_dlist(newid);
-        return 3;
-    }
+  rc = sqon_query (patts_get_db (), query, NULL, NULL);
+  sqon_free (query);
 
-    /* set info's id column to match return value of createUser */
-    strcpy(info->first->values[index], newid->first->values[0]);
-    cq_free_dlist(newid);
-
-    return cq_update(patts_get_db(), u8"User", info);
+  return rc;
 }
 
-int patts_create_task(struct dlist *info)
+int
+patts_create_task (const char *parentID, const char *displayName)
 {
-    int rc;
-    size_t index;
+  int rc;
+  const char *fmt = "INSERT INTO TaskType(parentID,displayName) "
+    "VALUES(%s,'%s')";
+  char *query;
+  size_t qlen = 1;
 
-    rc = cq_field_to_index(info, u8"id", &index);
-    if (!rc) { /* need to remove primary key from insertion */
-        rc = cq_dlist_remove_field_at(info, index);
-        if (rc) /* something is very very wrong */
-            return 1;
-    }
+  qlen += strlen (fmt) - 4;
+  qlen += strlen (parentID);
+  qlen += strlen (displayName);
 
-    return cq_insert(patts_get_db(), u8"TaskType", info);
+  query = sqon_malloc (qlen * sizeof (char));
+  if (NULL == query)
+    return PATTS_MEMORYERROR;
+
+  snprintf (query, qlen, fmt, parentID, displayName);
+
+  rc = sqon_query (patts_get_db (), query, NULL, NULL);
+  sqon_free (query);
+
+  return rc;
 }
 
-static int set_state(const char *table, const char *id, const char *state)
+static int
+set_state (const char *id, const char *table, const char *state)
 {
-    if (id == NULL)
-        return 1;
-    if (patts_fmaxlen() < 2) /* impending buffer overflow */
-        return -1;
+  int rc;
+  const char *fmt = "UPDATE %s SET state=%s WHERE id=%s";
+  char *query;
+  size_t qlen = 1;
 
-    struct dlist *list;
-    int rc = patts_get_user_byid(&list, id);
-    if (rc)
-        return rc;
-    if (cq_dlist_size(list) != 1) {
-        cq_free_dlist(list);
-    }
+  qlen += strlen (fmt) - 6;
+  qlen += strlen (id);
+  qlen += strlen (table);
+  qlen += strlen (state);
 
-    size_t index;
-    rc = cq_field_to_index(list, u8"state", &index);
-    if (rc) {
-        cq_free_dlist(list);
-        return rc;
-    }
+  query = sqon_malloc (qlen * sizeof (char));
+  if (NULL == query)
+    return PATTS_MEMORYERROR;
 
-    strcpy(list->first->values[index], state);
+  snprintf (query, qlen, fmt, table, state, id);
 
-    rc = cq_update(patts_get_db(), table, list);
-    cq_free_dlist(list);
+  rc = sqon_query (patts_get_db (), query);
+  sqon_free (query);
 
-    return rc;
+  return rc;
 }
 
-int patts_delete_user(const char *id)
+int
+patts_delete_user (const char *id)
 {
-    int rc;
-    char *query;
-    struct dlist *user = NULL;
-    const char *fmt = "mysqlUser FROM User WHERE id=%s";
-
-    query = calloc(patts_qlen(), sizeof(char));
-    if (NULL == query)
-        return -10;
-
-    rc = snprintf(query, patts_qlen(), fmt, id);
-    if (patts_qlen() <= (size_t) rc) {
-        free(query);
-        return 120;
-    }
-
-    rc = cq_select_query(patts_get_db(), &user, query);
-    free(query);
-    if (rc)
-        return 121;
-
-    rc = cq_revoke(patts_get_db(), u8"*", u8"*", user->first->values[0], u8"%",
-            u8"");
-    cq_free_dlist(user);
-    if (rc)
-        return 210;
-
-    return set_state(u8"User", id, u8"0");
+  return set_state (id, "User", "0");
 }
 
-int patts_delete_task(const char *id)
+int
+patts_delete_task (const char *id)
 {
-    return set_state(u8"TaskType", id, u8"0");
+  return set_state (id, "TaskType", "0");
 }
 
-int patts_grant_admin(const char *id)
+static int
+call_procedure (const char *proc, const char *args)
 {
-    const char *func = u8"grantAdmin";
-    const char *args[] = { id };
+  int rc;
+  const char *fmt = "CALL %s(%s)";
+  char *query;
+  size_t qlen = 1;
 
-    return cq_proc_arr(patts_get_db(), func, args, 1);
+  qlen += strlen (fmt) - 4;
+  qlen += strlen (proc);
+  qlen += strlen (args);
+
+  query = sqon_malloc (qlen * sizeof (char));
+  if (NULL == query)
+    return PATTS_MEMORYERROR;
+
+  snprintf (query, qlen, fmt, proc, args);
+
+  rc = sqon_query (patts_get_db (), query);
+  sqon_free (query);
+
+  return rc;
 }
 
-int patts_revoke_admin(const char *id)
+int
+patts_grant_admin (const char *id)
 {
-    const char *func = u8"revokeAdmin";
-    const char *args[] = { id };
+  return call_procedure ("grantAdmin", id);
+}
 
-    return cq_proc_arr(patts_get_db(), func, args, 1);
+int
+patts_revoke_admin (const char *id)
+{
+  return call_procedure ("revokeAdmin", id);
 }
