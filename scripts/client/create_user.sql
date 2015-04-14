94269df5dc66e5c2fe1811bed999021b935f2079
David McMackins II <contact@mcmackins.org>
2015-04-13 19:36:50 -0500

    Removed scripts that are now in the code

--- scripts/client/create_user.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER '$username'@'%' identified by '$password';
-GRANT INSERT, SELECT, UPDATE ON pattsdb.* TO '$username'@'%';
-INSERT INTO pattsdb.User(state,isAdmin,MysqlUser,firstName,lastName) VALUES(1,$adminbit,'$username','$firstname','$lastname');
-FLUSH PRIVILEGES;
